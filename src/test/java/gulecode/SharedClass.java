f4eac558518c3b42a667a86d9a297ac861b46a88
p123manager <managergit123@gmail.com>
2024-06-10 13:43:29 +0530

    shared class modified

--- src/test/java/gulecode/SharedClass.java
+++ src/test/java/gulecode/SharedClass.java
@@ -2,8 +2,14 @@ package gulecode;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SharedClass {
 	// open browser
+	@Test
+	public void m1() {
 	WebDriver driver = new ChromeDriver();
+	//lunch site
+	driver.get("http://www.facebook.com");
+	}
 }
