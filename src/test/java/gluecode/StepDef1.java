9034cfbe750344194979d23c1bf37290bc0f70a5
p123manager <managergit123@gmail.com>
2024-08-17 18:30:04 +0530

    message

--- src/test/java/gluecode/StepDef1.java
+++ src/test/java/gluecode/StepDef1.java
@@ -7,11 +7,19 @@ import org.testng.annotations.Test;
 public class StepDef1 {
 	@Test
 	public void m1() throws Exception{
+		String expected="facebook";
   WebDriver driver=new ChromeDriver();
   driver.get("http://www.facebook.com");
-  String title=driver.getTitle();
-  System.out.println(title);
+  String actual_title=driver.getTitle();
+  System.out.println(actual_title);
   Thread.sleep(3000);
+  if(actual_title.equals(actual_title)) {
+	  
+	  System.out.println("test case passed");
+  }else
+  {
+	  System.out.println("test case failed");
+  }
   driver.quit();
 	}
   
