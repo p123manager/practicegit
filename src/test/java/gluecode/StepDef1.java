3a7e1af48535f845eb1ed0a2ba29fec5ba46c73c
p123manager <managergit123@gmail.com>
2024-08-17 12:28:26 +0530

    msg

--- src/test/java/gluecode/StepDef1.java
+++ src/test/java/gluecode/StepDef1.java
@@ -8,7 +8,9 @@ public class StepDef1 {
 	@Test
 	public void m1() throws Exception{
   WebDriver driver=new ChromeDriver();
-  driver.get("http://www.gmail.com");
+  driver.get("http://www.facebook.com");
+  String title=driver.getTitle();
+  System.out.println(title);
   Thread.sleep(3000);
   driver.quit();
 	}
